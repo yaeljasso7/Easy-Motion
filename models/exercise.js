caa5ac398e7a0061199ed1c512ce5a25a4c07286
Ricardo Peralta <rcardo.peralta@gmail.com>
2018-10-02 15:04:40 -0500

    #17 Exercise, basic structure

---
+++ models/exercise.js
@@ -0,0 +1,30 @@
+class Exercise {
+  constructor({id, difficulty, executionTime, trainingType, bodyPart}) {
+    this.id = id;
+    this.difficulty = difficulty;
+    this.executionTime = executionTime;
+    this.trainingType = trainingType;
+    this.bodyPart = bodyPart;
+  }
+
+  static getExercises() {
+
+  }
+
+  static getExercise(exerciseId) {
+
+  }
+
+  static createExercise({difficulty, executionTime, trainingType, bodyPart}) {
+
+  }
+
+  static updateExercise(exerciseId, fields) {
+    
+  }
+
+  static deleteExercise(exerciseId) {
+
+  }
+
+}
