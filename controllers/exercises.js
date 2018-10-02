785c7c66191fb00c37a8467e8038638805a71658
Ricardo Peralta <rcardo.peralta@gmail.com>
2018-10-02 15:35:51 -0500

    #18 Exercises controller, basic structure

---
+++ controllers/exercises.js
@@ -0,0 +1,33 @@
+const { Exercise } = require('../models');
+
+class ExerciseCtrl {
+  constructor() {
+    this.getAll = this.getAll.bind(this);
+    this.get = this.get.bind(this);
+    this.create = this.create.bind(this);
+    this.delete = this.delete.bind(this);
+    this.update = this.update.bind(this);
+  }
+
+  getAll(req, res) {
+
+  }
+
+  get(req, res) {
+
+  }
+
+  create(req, res) {
+
+  }
+
+  update(req, res) {
+
+  }
+
+  delete(req, res) {
+
+  }
+}
+
+module.exports = new ExerciseCtrl();
