ddcb39576041989fc9ba3f12be0fb2e4d69b217a
Ricardo Peralta <rcardo.peralta@gmail.com>
2018-10-02 18:09:17 -0500

    #19: missing PUT param

--- routes/exercises.js
+++ routes/exercises.js
@@ -4,7 +4,7 @@ const { exercisesCtrl } = require('../controllers');
 router.get('/', exercisesCtrl.getAll);
 router.get('/:exerciseId', exercisesCtrl.get);
 router.post('/', exercisesCtrl.create);
-router.put('/', exercisesCtrl.update);
+router.put('/:exerciseId', exercisesCtrl.update);
 router.delete('/:exerciseId', exercisesCtrl.delete);
 
 module.exports = router;
