404d0b07a6c096eca271fb08b3e8067cc6326b80
iChrisx10x <christopherx10x@gmail.com>
2018-10-01 21:15:14 -0500

    test dbmyswlfree

--- db/index.js
+++ db/index.js
@@ -1,14 +1,13 @@
 const mysql = require('mysql');
-//const User = require('../models/user');
 
 class DB{
 
   constructor(){
     this.con = mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME
+      host: 85.10.205.173:3306,
+      user: ichris96,
+      password: 12345678,
+      database: easymotionsql
     });
 
 //    this.con.connect();
@@ -21,99 +20,9 @@ class DB{
     });
   }
 
-  getAll(table){
-    return new Promise((resolve, reject) => {
-      this.con.query('SELECT * FROM ??', [table], (error, results) => {
-        if (error) return reject(this.processError(error));
-        return resolve(results);
-    });
-   });
+  query(){
+
   }
-
-  get(table, id) {
-    return new Promise((resolve, reject) => {
-      this.con.query('SELECT * FROM ?? WHERE id = ?', [table, id], (error, results) => {
-        if (error) return reject(this.processError(error));
-        console.log("db-get",results);
-        return resolve(results);
-      });
-    });
-  }
-
-  delete(table, id) {
-    return new Promise((resolve, reject) => {
-      this.con.query('DELETE FROM ?? WHERE id = ?', [table, id], (error, results) => {
-        if (error) {
-            //error de la base de datos como mail repetido...
-          let err = this.processError(error);
-          return reject(err);
-        }
-      //  console.log("db-delete",results);
-        return resolve(results);
-      });
-    });
-  }
-
-  update(table, obj, id) {
-    console.log(obj);
-    console.log(id);
-    console.log(table);
-    return new Promise((resolve, reject) => {
-      this.con.query('UPDATE ?? SET ? WHERE id = ?', [table, obj, id], (error, results) => {
-        if (error) {
-          let err = this.processError(error);
-          return reject(err);
-        }
-        resolve(results);
-      //  console.log(results);
-      });
-    });
-  }
-
-  insert(table, resource) {
-    console.log(resource); //{ name: juan, mobile: 21421, }
-    return new Promise((resolve, reject) => {
-      this.con.query('INSERT INTO ?? SET ?', [table, resource], (error, results) => {
-        if (error) {
-            //error de la base de datos como mail repetido...
-          console.log(error);
-          let err = this.processError(error);
-          return reject(err);
-        }
-        return resolve(results);
-      });
-    });
-  }
-
-  processError(err) {
-    console.log("soy error");
-    const error = {};
-
-    switch (err.code) {
-      case 'ER_DUP_ENTRY':
-        let data = this.getDataFromErrorMsg(err.sqlMessage);
-        error['duplicated'] = {
-          message: `The ${data.field} ${data.data} already exists on the system`,
-          field: data.field,
-          sql: err.sql,
-        };
-        break;
-      default:
-
-    }
-
-    return error;
-  }
-
-  getDataFromErrorMsg(message) {
-    let data = unescape(message).match(/'([^']+)'/g)
-    return {
-      field: data[1].slice(1,-1),
-      data: data[0].slice(1,-1),
-    }
-  }
-
-
 
 }
 
