20d18a760885916400d829d7bd9173f95a018342
Ricardo Peralta <rcardo.peralta@gmail.com>
2018-10-04 20:32:01 -0500

    #22: add bodyParts routes to index

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const exercisesRouter = require('./exercises');
+const bodyPartsRouter = require('./bodyParts');
 
 router.get('/', (req, res) => res.send('hello'));
 
 router.use('/users', usersRouter);
 
 router.use('/exercises', exercisesRouter);
+
+router.use('/bodyParts', bodyPartsRouter);
 
 module.exports = router;
