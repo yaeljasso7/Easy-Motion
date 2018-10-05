bc6b7c69469adc8cfdf719910d4ae0d144bc7fc2
Ricardo Peralta <rcardo.peralta@gmail.com>
2018-10-04 20:27:19 -0500

    #20: getAll, getById, create, update bodyParts w/ DB connection

---
+++ models/bodyPart.js
@@ -0,0 +1,61 @@
+const db = require('../db');
+
+class BodyPart {
+  constructor({
+    id, name,
+  }) {
+    this.id = id;
+    this.name = name;
+  }
+
+  static async getAll() {
+    const data = await db.selectAll('body_parts');
+    const response = [];
+    data.forEach((row) => {
+      response.push(new BodyPart(row));
+    });
+    return response;
+  }
+
+  static async get(BodyPartId) {
+    const data = await db.select('body_parts', BodyPartId);
+    return data.length !== 0 ? new BodyPart(data[0]) : [];
+  }
+
+  static async create({
+    name,
+  }) {
+    let response;
+    try {
+      response = await db.insert('body_parts', {
+        name,
+      });
+    } catch (err) {
+      throw err;
+    }
+
+    const id = response.insertId;
+    if (id > 0) {
+      return new BodyPart({
+        id, name,
+      });
+    }
+    return [];
+  }
+
+  static async update(bodyPartId, fields) {
+    let res;
+    try {
+      res = await db.update('body_parts', fields, bodyPartId);
+    } catch (err) {
+      throw err;
+    }
+    return res.affectedRows > 0;
+  }
+
+  static delete(bodyPartId) {
+
+  }
+}
+
+module.exports = BodyPart;
