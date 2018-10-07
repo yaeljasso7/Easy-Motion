f4a40497b53c30878c4434c4568e0db751b54eab
Ricardo Peralta <rcardo.peralta@gmail.com>
2018-10-06 20:20:42 -0500

    #39: add routines controller to index

--- controllers/index.js
+++ controllers/index.js
@@ -2,3 +2,4 @@
 exports.exercisesCtrl = require('./exercises');
 exports.bodyPartsCtrl = require('./bodyParts');
 exports.trainingTypesCtrl = require('./trainingTypes');
+exports.routinesCtrl = require('./routines');
