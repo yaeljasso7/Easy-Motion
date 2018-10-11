862b16df5620a93438150cb9014a2bf338d1e80f
Ricardo Peralta <rcardo.peralta@gmail.com>
2018-10-10 19:02:58 -0500

    #38 #39 #40 manage routine-exercises

--- models/routine.js
+++ models/routine.js
@@ -1,4 +1,5 @@
 const db = require('../db');
+const Exercise = require('./exercise');
 
 class Routine {
   constructor({id, name})
@@ -8,7 +9,7 @@ class Routine {
   }
 
   static async getAll() {
-    const data = await db.getAll('routine');
+    const data = await db.select('routine');
     const response = [];
     data.forEach((row) => {
       response.push(new Routine(row));
@@ -17,13 +18,11 @@ class Routine {
   }
 
   static async get(routineId) {
-    const data = await db.get('routine', routineId);
+    const data = await db.select('routine', { id: routineId });
     return data.length !== 0 ? new Routine(data[0]) : data;
   }
 
-  static async create({
-    name,
-  }) {
+  static async create({ name }) {
     let response;
     try {
       response = await db.insert('routine', {
@@ -65,7 +64,47 @@ class Routine {
     return deletedRows > 0;
   }
 
+  static async addExercise(routineId, exerciseId) {
+    const idRoutine = routineId;
+    const idExercise = exerciseId;
+    let response;
+    try {
+      response = await db.insert('exerciseOnRoutine', { idRoutine, idExercise });
+    } catch (err) {
+      throw err;
+    }
 
+    const id = response.insertId;
+    if (response.affectedRows > 0) {
+      return { routineId, exerciseId };
+    }
+    return [];
+  }
+
+  static async getExercises(routineId) {
+    const data = await db.select('exerciseOnRoutine', { idRoutine: routineId });
+    const response = [];
+    const myPromises = data.map(async (row) => {
+      const exercise = await Exercise.get(row.idExercise);
+      response.push(exercise);
+    });
+    await Promise.all(myPromises);
+    return response;
+  }
+
+  static async removeExercise(routineId, exerciseId) {
+    const idRoutine = routineId;
+    const idExercise = exerciseId;
+    let deletedRows;
+    try {
+      const results = await db.adv_delete('exerciseOnRoutine', { idRoutine, idExercise });
+      deletedRows = results.affectedRows;
+    } catch (e) {
+      throw e;
+    }
+
+    return deletedRows > 0;
+  }
 
 }
 
--- routes/routines.js
+++ routes/routines.js
@@ -3,16 +3,20 @@ const { routinesCtrl } = require('../controllers');
 const middlewares = require('../middlewares');
 
 router.get('/', routinesCtrl.getAll);
-//router.get('/:routineId', routinesCtrl.get);
-router.get('/:routineId', (req,res,next) => {
+router.get('/:routineId', routinesCtrl.get);
+/*router.get('/:routineId', (req,res,next) => {
   middlewares.validator.validate(req, res, next, {
   params: {
     routineId: 'number',
   },
 });
-}, routinesCtrl.get);
+}, routinesCtrl.get);*/
 router.post('/', routinesCtrl.create);
 router.put('/:routineId', routinesCtrl.update);
 router.delete('/:routineId', routinesCtrl.delete);
+
+router.post('/:routineId/exercises', routinesCtrl.addExercise);
+router.get('/:routineId/exercises', routinesCtrl.getExercises);
+router.delete('/:routineId/exercises/:exerciseId', routinesCtrl.removeExercise);
 
 module.exports = router;
