639d7887b6da7b68e04e836f8e2e1a103143a505
Ricardo Peralta <rcardo.peralta@gmail.com>
2018-10-11 22:50:15 -0500

    close #71 - Basic middlewares

--- routes/bodyParts.js
+++ routes/bodyParts.js
@@ -1,10 +1,42 @@
 const router = require('express').Router();
 const { bodyPartsCtrl } = require('../controllers');
+const middlewares = require('../middlewares');
 
 router.get('/', bodyPartsCtrl.getAll);
-router.get('/:bodyPartId', bodyPartsCtrl.get);
-router.post('/', bodyPartsCtrl.create);
-router.put('/:bodyPartId', bodyPartsCtrl.update);
-router.delete('/:bodyPartId', bodyPartsCtrl.delete);
+
+router.get('/:bodyPartId', (req, res, next) => {
+  middlewares.validator.validate(req, res, next, {
+    params: {
+      trainingTypeId: 'number',
+    },
+  });
+}, bodyPartsCtrl.get);
+
+router.post('/', (req, res, next) => {
+  middlewares.validator.validate(req, res, next, {
+    body: {
+      name: 'word,required',
+    },
+  });
+}, bodyPartsCtrl.create);
+
+router.put('/:bodyPartId', (req, res, next) => {
+  middlewares.validator.validate(req, res, next, {
+    body: {
+      name: 'word,required',
+    },
+    params: {
+      trainingTypeId: 'number',
+    },
+  });
+},  bodyPartsCtrl.update);
+
+router.delete('/:bodyPartId', (req, res, next) => {
+  middlewares.validator.validate(req, res, next, {
+    params: {
+      trainingTypeId: 'number',
+    },
+  });
+}, bodyPartsCtrl.delete);
 
 module.exports = router;
