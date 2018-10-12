f0383a1aff8eed544b277ee9fecce8ccb4f24124
Ricardo Peralta <rcardo.peralta@gmail.com>
2018-10-11 22:51:08 -0500

    close #74 - Basic middlewares

--- routes/routines.js
+++ routes/routines.js
@@ -3,20 +3,68 @@ const { routinesCtrl } = require('../controllers');
 const middlewares = require('../middlewares');
 
 router.get('/', routinesCtrl.getAll);
-router.get('/:routineId', routinesCtrl.get);
-/*router.get('/:routineId', (req,res,next) => {
-  middlewares.validator.validate(req, res, next, {
-  params: {
-    routineId: 'number',
-  },
-});
-}, routinesCtrl.get);*/
-router.post('/', routinesCtrl.create);
-router.put('/:routineId', routinesCtrl.update);
-router.delete('/:routineId', routinesCtrl.delete);
 
-router.post('/:routineId/exercises', routinesCtrl.addExercise);
-router.get('/:routineId/exercises', routinesCtrl.getExercises);
-router.delete('/:routineId/exercises/:exerciseId', routinesCtrl.removeExercise);
+router.get('/:routineId', (req, res, next) => {
+  middlewares.validator.validate(req, res, next, {
+    params: {
+      routineId: 'number',
+    },
+  });
+}, routinesCtrl.get);
+
+router.post('/', (req, res, next) => {
+  middlewares.validator.validate(req, res, next, {
+    body: {
+      name: 'word,required',
+    },
+  });
+}, routinesCtrl.create);
+
+router.put('/:routineId', (req, res, next) => {
+  middlewares.validator.validate(req, res, next, {
+    body: {
+      name: 'word,required',
+    },
+    params: {
+      routineId: 'number',
+    },
+  });
+}, routinesCtrl.update);
+
+router.delete('/:routineId', (req, res, next) => {
+  middlewares.validator.validate(req, res, next, {
+    params: {
+      routineId: 'number',
+    },
+  });
+}, routinesCtrl.delete);
+
+router.post('/:routineId/exercises', (req, res, next) => {
+  middlewares.validator.validate(req, res, next, {
+    body: {
+      exerciseId: 'number,required',
+    },
+    params: {
+      routineId: 'number',
+    },
+  });
+}, routinesCtrl.addExercise);
+
+router.get('/:routineId/exercises', (req, res, next) => {
+  middlewares.validator.validate(req, res, next, {
+    params: {
+      routineId: 'number',
+    },
+  });
+}, routinesCtrl.getExercises);
+
+router.delete('/:routineId/exercises/:exerciseId', (req, res, next) => {
+  middlewares.validator.validate(req, res, next, {
+    params: {
+      routineId: 'number',
+      exerciseId: 'number',
+    },
+  });
+}, routinesCtrl.removeExercise);
 
 module.exports = router;
