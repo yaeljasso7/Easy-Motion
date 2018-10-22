c77ce76350448e27281ff2b41ee1fbac1c2f8445
Ricardo Peralta <rcardo.peralta@gmail.com>
2018-10-22 02:19:20 -0500

    match permissions w/ database

--- routes/bodyParts.js
+++ routes/bodyParts.js
@@ -12,43 +12,36 @@ router.get('/:bodyPartId', (req, res, next) => {
   });
 }, bodyPartsCtrl.get);
 
-router.post('/', [auth.haveSession,
+router.use('/', auth.haveSession,
   (req, res, next) => {
-    auth.havePermission(req, res, next, 'createBodyPart');
-  },
-  (req, res, next) => {
-    validator.validate(req, res, next, {
-      body: {
-        name: 'word,required',
-      },
-    });
-  }], bodyPartsCtrl.create);
+    auth.havePermission(req, res, next, 'manageBodyParts');
+  });
 
-router.put('/:bodyPartId', [auth.haveSession,
-  (req, res, next) => {
-    auth.havePermission(req, res, next, 'editBodyPart');
-  },
-  (req, res, next) => {
-    validator.validate(req, res, next, {
-      body: {
-        name: 'word,required',
-      },
-      params: {
-        bodyPartId: 'number',
-      },
-    });
-  }], bodyPartsCtrl.update);
+router.post('/', [(req, res, next) => {
+  validator.validate(req, res, next, {
+    body: {
+      name: 'word,required',
+    },
+  });
+}], bodyPartsCtrl.create);
 
-router.delete('/:bodyPartId', [auth.haveSession,
-  (req, res, next) => {
-    auth.havePermission(req, res, next, 'deleteBodyPart');
-  },
-  (req, res, next) => {
-    validator.validate(req, res, next, {
-      params: {
-        bodyPartId: 'number',
-      },
-    });
-  }], bodyPartsCtrl.delete);
+router.put('/:bodyPartId', (req, res, next) => {
+  validator.validate(req, res, next, {
+    body: {
+      name: 'word,required',
+    },
+    params: {
+      bodyPartId: 'number',
+    },
+  });
+}, bodyPartsCtrl.update);
+
+router.delete('/:bodyPartId', (req, res, next) => {
+  validator.validate(req, res, next, {
+    params: {
+      bodyPartId: 'number',
+    },
+  });
+}, bodyPartsCtrl.delete);
 
 module.exports = router;
